3c35f88bb642a314d1aa6aafedcc10fad0054f46
Jérôme Ludmann <jerome@ludmann.io>
2016-09-18 16:47:13 +0200

    Adding ES2017 promise/async/await tests

--- src/ES2017-promise-async-await.js
+++ src/ES2017-promise-async-await.js
@@ -1,19 +1,14 @@
-import debug from 'debug';
-import increment from './lib/increment';
-import promisify from 'es6-promisify';
-
-const log = debug('asyncjs:promise-async-await');
-const incrementPromise = promisify(increment);
+import increment from './lib/increment-promise';
 
 export default async function (givenValue) {
-  try {
-    let incremented = await incrementPromise(givenValue); // first increment
-    incremented = await incrementPromise(incremented); // second increment
-    incremented = await incrementPromise(incremented); // third increment
 
-    log(incremented);
+  try {
+    let incremented = await increment(givenValue); // first increment
+    incremented = await increment(incremented); // second increment
+    return increment(incremented); // third increment
 
   } catch (err) {
-    log(err.message);
+    return err;
   }
-};
+
+}
