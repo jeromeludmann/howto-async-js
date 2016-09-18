7e70d6966a990f3e9c32a3d837caec6489bc09f8
Jérôme Ludmann <jerome@ludmann.io>
2016-09-18 16:46:54 +0200

    Adding ES2015 promise/generator/yield tests

--- src/ES2015-promise-generator.js
+++ src/ES2015-promise-generator.js
@@ -1,20 +1,13 @@
-import debug from 'debug';
 import co from 'co';
-import increment from './lib/increment';
-import promisify from 'es6-promisify';
-
-const log = debug('asyncjs:promise-generator');
-const incrementPromise = promisify(increment);
+import increment from './lib/increment-promise';
 
 export default function (givenValue) {
-  co(function * () {
-    let incremented = yield incrementPromise(givenValue); // first increment
-    incremented = yield incrementPromise(incremented); // second increment
-    incremented = yield incrementPromise(incremented); // third increment
 
-    log(incremented);
+  return co(function * () {
+    let incremented = yield increment(givenValue); // first increment
+    incremented = yield increment(incremented); // second increment
+    return increment(incremented); // third increment
 
-  }).catch(function (err) {
-    log(err.message);
-  });
-};
+  }).catch((err) => err);
+
+}
